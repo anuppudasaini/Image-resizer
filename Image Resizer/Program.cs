9348ca704673d5c61d91ffeb2abcf63b7ae9519b
Anup pudasaini <54308126+anuppudasaini@users.noreply.github.com>
2024-07-30 11:27:24 +1000

    Add files via upload

---
+++ Image Resizer/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/// <summary>
+/// Provides classes and methods for resizing images.
+/// </summary>
+/// namespace Image_Resizer
+{
+    static class Program
+{
+
+    [STAThread]
+    static void Main()
+    {
+        Application.EnableVisualStyles();
+        Application.SetCompatibleTextRenderingDefault(false);
+        Application.Run(new Form1());
+    }
+}
+}
